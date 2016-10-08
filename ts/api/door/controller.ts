8b630ea4701e1fab8e16a4979219e6479ca0c0b3
Dolan Miu <dolan_miu@hotmail.com>
2016-10-08 17:57:18 +0100

    added cool down feature

--- ts/api/door/controller.ts
+++ ts/api/door/controller.ts
@@ -3,6 +3,7 @@ import * as PythonShell from "python-shell";
 import { State, DoorState } from "../../state";
 
 var state = new State();
+var onCooldown: boolean = false;
 
 export function index(req: Request, res: Response) {
     res.status(200).send(state.GetDoorBinary().toString());
@@ -13,11 +14,17 @@ export function open(req: Request, res: Response) {
         res.status(500).send("Door already open");
     }
 
+    if (onCooldown) {
+        res.status(500).send("Sending commands too fast. Please wait");
+    }
+
     PythonShell.run("open-door.py", { scriptPath: __dirname + '/../py' }, (err: Error, result: any) => {
         if (err) {
             res.status(500).send("Script had an error: " + err);
             return;
         }
+
+        CommenceCoolDown();
 
         state.OpenDoor();
         res.status(200);
@@ -29,13 +36,26 @@ export function close(req: Request, res: Response) {
         res.status(500).send("Door already closed");
     }
 
+    if (onCooldown) {
+        res.status(500).send("Sending commands too fast. Please wait");
+    }
+
     PythonShell.run("close-door.py", { scriptPath: __dirname + '/../py' }, (err: Error, result: any) => {
         if (err) {
             res.status(500).send("Script had an error: " + err);
             return;
         }
 
+        CommenceCoolDown();
+
         state.CloseDoor();
         res.status(200);
     });
 };
+
+function CommenceCoolDown(): void {
+    onCooldown = true;
+    setTimeout(() => {
+        onCooldown = false;
+    }, 10000);
+}
