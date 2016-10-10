fc6052c06f42c492d650b2f343817a0e3ca68434
Dolan Miu <dolan_miu@hotmail.com>
2016-10-11 00:58:46 +0100

    using app root path

--- ts/api/door/controller.ts
+++ ts/api/door/controller.ts
@@ -1,6 +1,7 @@
 import {RequestHandler, Request, Response} from "express";
 import * as PythonShell from "python-shell";
 import { State, DoorState } from "../../state";
+import * as appRoot from 'app-root-path';
 
 var state = new State();
 var onCooldown: boolean = false;
@@ -18,7 +19,7 @@ export function open(req: Request, res: Response) {
         res.status(500).send("Sending commands too fast. Please wait");
     }
 
-    PythonShell.run("open-door.py", { scriptPath: __dirname + '/../py' }, (err: Error, result: any) => {
+    PythonShell.run("open-door.py", { scriptPath: appRoot.path + '/py' }, (err: Error, result: any) => {
         if (err) {
             res.status(500).send("Script had an error: " + err);
             return;
@@ -40,7 +41,7 @@ export function close(req: Request, res: Response) {
         res.status(500).send("Sending commands too fast. Please wait");
     }
 
-    PythonShell.run("close-door.py", { scriptPath: __dirname + '/../py' }, (err: Error, result: any) => {
+    PythonShell.run("close-door.py", { scriptPath: appRoot.path + '/py' }, (err: Error, result: any) => {
         if (err) {
             res.status(500).send("Script had an error: " + err);
             return;
