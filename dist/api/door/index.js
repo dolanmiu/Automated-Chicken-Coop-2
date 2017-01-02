4fe148679ca7da07abadb3f1106124cc3c4a13cd
Dolan Miu <dolan_miu@hotmail.com>
2017-01-02 02:27:57 +0000

    removed list folder

--- dist/api/door/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const express_1 = require("express");
-const controller = require("./controller");
-var router = express_1.Router();
-exports.router = router;
-router.get("/", controller.index);
-router.get("/open", controller.open);
-router.get("/close", controller.close);
